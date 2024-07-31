c4a30835a2ad889175b8ec98a60242008c2c0dec
SAKTHIPRIYAN1 <sakthipriyan417@gmail.com>
2024-07-31 22:26:22 +0530

    commit for cli final

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL,
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  build: {
+    outDir: 'dist'
+  }
 });
